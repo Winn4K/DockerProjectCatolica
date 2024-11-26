6e3277f5a5332ea4f96e9d2fea4c6e338a4e49b8
Winn4K <kauan99436601@gmail.com>
2024-11-25 22:08:22 -0300

    changing paths

--- my-web-app/public/index.js
+++ my-web-app/public/index.js
@@ -3,18 +3,18 @@ const imgLampada = document.getElementById("imgLampada");
 var lampadaQuebrada = false;
 
 function quebrarLampada() {
-    imgLampada.setAttribute('src', '/styles/imgs/lampadaQuebrada.svg')
+    imgLampada.setAttribute('src', 'styles/imgs/lampadaQuebrada.svg')
     lampadaQuebrada = true;
 }
 
 function ligarLampada() {
     if (lampadaQuebrada == false) {
-        imgLampada.setAttribute('src', '/styles/imgs/lampadaAcesa.svg')
+        imgLampada.setAttribute('src', 'styles/imgs/lampadaAcesa.svg')
     }
 }
 
 function apagarLampada() {
     if (lampadaQuebrada == false) {
-        imgLampada.setAttribute('src', '/styles/imgs/lampadaApagada.svg')
+        imgLampada.setAttribute('src', 'styles/imgs/lampadaApagada.svg')
     }
 }
